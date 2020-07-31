40f4f5d293348922a97fb79e5817649a7b4a2970
David Salgado <david@digitalronin.com>
2020-07-31 12:47:22 +0100

    get nat gateway ids from terraform.tfstate

--- foo.rb
+++ foo.rb
@@ -1,17 +1,28 @@
 #!/usr/bin/env ruby
 
 require "pry-byebug"
+require "json"
 require "bundler/setup"
 require "aws-sdk-ec2"
 
 def nat_gateway_ids_for_vpc(client, vpc_id)
   filter = [ { name: "vpc-id", values: [vpc_id] } ]
   data = client.describe_nat_gateways(filter: filter)
-  data.nat_gateways.map { |ng| ng.nat_gateway_id }
+  data.nat_gateways.map { |ng| ng.nat_gateway_id }.sort
 end
 
-ec2 = Aws::EC2::Client.new(region:'eu-west-2', profile: ENV["AWS_PROFILE"])
+def nat_gateway_ids_from_terraform_state(statefile)
+  str = File.read("terraform.tfstate")
+  data = JSON.parse(str)
+  list = data["resources"]
+  nat_gateway = list.filter { |m| m["name"] == "private_nat_gateway" }.first
+  nat_gateway["instances"].map { |ng| ng.dig("attributes", "nat_gateway_id") }.sort
+end
 
 # binding.pry
 
+ec2 = Aws::EC2::Client.new(region:'eu-west-2', profile: ENV["AWS_PROFILE"])
 pp nat_gateway_ids_for_vpc(ec2, "vpc-0c16457fd570a1f0b")
+
+pp nat_gateway_ids_from_terraform_state("terraform.tfstate")
+
